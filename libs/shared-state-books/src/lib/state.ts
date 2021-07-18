eb6ac97b05f10fa910f9582c1d50c4d8a84e6b22
Mike Ryan <mike.ryan52@gmail.com>
2021-07-17 19:32:16 -0500

    04-setting-up-the-store

--- libs/shared-state-books/src/lib/state.ts
+++ libs/shared-state-books/src/lib/state.ts
@@ -13,9 +13,13 @@ export const FEATURE_KEY = 'shared-books';
 /**
  * State Shape
  **/
-export interface State {}
+export interface State {
+  books: fromBooks.State;
+}
 
-export const reducers: ActionReducerMap<State> = {};
+export const reducers: ActionReducerMap<State> = {
+  books: fromBooks.reducer,
+};
 
 export const metaReducers: MetaReducer<State>[] = [];
 
